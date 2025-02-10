1b51dc5f58f1f5ef4935a7351420125fceea7e0c
Erick De Greef <12631133+erick207@users.noreply.github.com>
2025-02-10 10:38:25 -0300

    add exercise 006

---
+++ src/chapter01/exercise006/Exercise006.java
@@ -0,0 +1,10 @@
+package chapter01.exercise006;
+
+public class Exercise006 {
+    public static void main(String[] args) {
+        var text = """
+                She told him: \n\t"I'm going to the mall."\
+                """;
+        System.out.println(text);
+}
+}
