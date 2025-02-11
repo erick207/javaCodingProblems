2b0a07e52b611f12e158eab0a1541e3d3328c103
Erick De Greef <12631133+erick207@users.noreply.github.com>
2025-02-11 09:12:04 -0300

    add exercise 009

---
+++ src/chapter01/exercise009/Exercise009.java
@@ -0,0 +1,11 @@
+package chapter01.exercise009;
+
+public class Exercise009 {
+    public static void main(String[] args) {
+        /* adding comments in a text block is NOT supported by the spec
+         * one could improvise adding them and then using
+         * String.replace with a regex to remove them
+         * but it's better to not do it
+        */
+    }
+}
